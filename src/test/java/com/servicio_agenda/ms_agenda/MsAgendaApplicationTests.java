024473c7210c15da9448c71c7946fb8d6cdc2241
PabloDev <pe.philimon@duocuc.cl>
2026-05-16 15:49:53 -0400

    creación del nuevo microservicio agenda medica

---
+++ src/test/java/com/servicio_agenda/ms_agenda/MsAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio_agenda.ms_agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
