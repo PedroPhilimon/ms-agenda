12b46df800ba2ddc7ced1e1b04a2ae9f1dde24a7
Ewann777 <evancesar.markez@gmail.com>
2026-05-17 16:37:20 -0400

    Agregué AgendaMedico en model

---
+++ src/main/java/com/servicio_agenda/ms_agenda/model/AgendaMedico.java
@@ -0,0 +1,23 @@
+package com.servicio_agenda.ms_agenda.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "agendas_medicos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgendaMedico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAgenda;
+    
+    private Long idMedico;
+    private LocalDateTime fechaHoraInicio;
+    private LocalDateTime fechaHoraFin;
+    private String estado;
+}
